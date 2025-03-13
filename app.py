f6c86d6cb6b5c27db06e7c911eb51ad3bae4ad95
Mbabazi-Hawa-Bint-Edrisa <143553200+Mbabazi-Hawa-Bint-Edrisa@users.noreply.github.com>
2025-03-13 16:39:30 +0300

    Linear regression

--- app.py
+++ app.py
@@ -1,48 +1,43 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-application=Flask(__name__)
-
-app=application
+application = Flask(__name__)
+app = application
 
 ## Route for a home page
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('home.html')
     else:
-        data=CustomData(
-            Item_Weight = request.form.get("Item_Weight"),
-            Item_Fat_Content = request.form.get('Item_Fat_Content'),
-            Item_Type = request.form.get('Item_Type'),
-            Item_MRP = request.form.get('Item_MRP'),
-            Outlet_Size = request.form.get('Outlet_Size'),
-            Outlet_Location_Type = request.form.get('Outlet_Location_Type'),
-            Outlet_Type = request.form.get('Outlet_Type'),
-            # Outlet_Age = request.form.get('Outlet_Age')
-            
-            )
+        data = CustomData(
+            Item_Weight=request.form.get("Item_Weight"),
+            Item_Fat_Content=request.form.get('Item_Fat_Content'),
+            Item_Type=request.form.get('Item_Type'),
+            Item_MRP=request.form.get('Item_MRP'),
+            Outlet_Size=request.form.get('Outlet_Size'),
+            Outlet_Location_Type=request.form.get('Outlet_Location_Type'),
+            Outlet_Type=request.form.get('Outlet_Type'),
+            # Outlet_Age=request.form.get('Outlet_Age')
+        )
 
-        pred_df=data.get_data_as_data_frame()
+        pred_df = data.get_data_as_data_frame()
         print(pred_df)
 
-        predict_pipeline=PredictPipeline()
-        results=predict_pipeline.predict(pred_df)
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
         # Round up the result to 2 decimal places and include the US dollar symbol
         return render_template('home.html', results=f'Prediction based on your inputs is ${results[0]:.2f}')
 
-    
-
-if __name__=="__main__":
-    app.run(debug= True)        
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
+   
     
 
--- setup.py
+++ setup.py
@@ -1,31 +1,39 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
-
+import os
 
 HYPHEN_DOT_E = "-e ."
-def get_requirements(file_path:str)->List[str]:
-
+def get_requirements(file_path: str) -> List[str]:
     '''
     This function will return the list of requirements
-
     '''
     requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-
-        if HYPHEN_DOT_E in requirements:
-            requirements.remove(HYPHEN_DOT_E)
+    try:
+        with open(file_path) as file_obj:
+            requirements = file_obj.readlines()
+            requirements = [req.replace("\n", "") for req in requirements]
+            if HYPHEN_DOT_E in requirements:
+                requirements.remove(HYPHEN_DOT_E)
+    except FileNotFoundError:
+        print(f"Warning: {file_path} not found. Using empty requirements list.")
     return requirements
 
 setup(
-
-name = 'Sales Prediction End to End Project',
-version = '0.0.1',
-author = 'Ayodele Ayodeji',
-author_email = 'ayodeleayodeji250@gmail.com',
-packages = find_packages(),
-install_requires = get_requirements('requirements.txt')
-
-
+    name='Sales Prediction Project',
+    version='0.1.0',
+    author='Mbabazi Hawa Bint Edrisa',
+    author_email='mbabaziedrisa0@gmail.com',
+    description='A project to predict sales using machine learning models',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=get_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt')),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
 )
